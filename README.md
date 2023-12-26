be7dac39977245f7398a26b9d3442f4fe4fa24c8
Carole Gérard <gerard.carole78@gmail.com>
2023-12-26 18:16:02 +0100

    handleChangeQuestion

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # the-impossible-quiz
+Quiz conducted as part of the TypeScript learning process during my training at BeCode.
 
+## Tech
 React + TypeScript + Vite
+
+## Deployment
+https://the-impossible-quiz-react-typescript.vercel.app/
--- src/components/question/Question.tsx
+++ src/components/question/Question.tsx
@@ -12,19 +12,22 @@ const Question = () => {
     const [currentQuestion, setCurrentQuestion] = useState(allQuestions[0]);
     const [hint, setHint] = useState(false);
     
-    
-    function handleNextQuestion(e: MouseEvent<HTMLButtonElement>) {
+
+    /**
+     * Handles the change of the current question based on the button clicked.
+     * @param {string} incr - The direction of the question change, either 'next' or 'prev'.
+     * @param {MouseEvent<HTMLButtonElement>} e - The mouse event triggered by the button click.
+     */
+    function handleChangeQuestion(incr: string, e: MouseEvent<HTMLButtonElement>) {
       e.preventDefault();
-      setHint(false);
-      if (index < maxIndex) {
-        setIndex((prevIndex) => {
-          setCurrentQuestion(allQuestions[prevIndex + 1]);
-          return prevIndex + 1;
-        });
-      } else {
-        setIndex(0);
-        setCurrentQuestion(allQuestions[0]);
-      }
+
+      // Calculate the new index based on the button click
+      const newIndex =
+        incr === 'next' ? (index + 1) % (maxIndex + 1) : (index - 1 + maxIndex + 1) % (maxIndex + 1);
+
+      // Update the index and set the current question based on the new index
+      setIndex(newIndex);
+      setCurrentQuestion(allQuestions[newIndex]);
     }
   
     function handleHint() {
@@ -58,7 +61,8 @@ const Question = () => {
               ))}
           </>
         )}  
-        <button onClick={handleNextQuestion}>Next</button>
+        <button onClick={(e) => handleChangeQuestion("prev", e)}>Previous</button>
+        <button onClick={(e) => handleChangeQuestion("next", e)}>Next</button>
         <button onClick={handleHint}>Hint</button>
       </div>
     
